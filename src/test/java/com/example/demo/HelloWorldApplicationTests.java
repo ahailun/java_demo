3bd32b21530b5754723c93abecd9bf4ec3b494e2
lianghailun <ahailun@126.com>
2021-02-16 17:54:27 +0800

    用例问题

--- src/test/java/com/example/demo/HelloWorldApplicationTests.java
+++ src/test/java/com/example/demo/HelloWorldApplicationTests.java
@@ -5,9 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class HelloWorldApplicationTests {
-
 	@Test
 	void contextLoads() {
+		assertEquals(true,true);
 	}
 
 }
