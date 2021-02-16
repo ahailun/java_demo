345ca8c4a2544c18ee625cd6e09e968ebee43654
lianghailun <ahailun@126.com>
2021-02-16 17:27:44 +0800

    修改语法错误

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,12 @@
-package cn.ly.helloWorld;
+package org.wl.demo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@RestController
+@SpringBootApplication
+public class HelloWorldApplication {
 
-public class HelloController {
-    @RequestMapping("/hello")
-    public String hello(){
-        return "Hello World!";
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
     }
 }
