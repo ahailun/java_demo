a64bed7447ba125a1aeb42d24ab838024b62fd13
lianghailun <ahailun@126.com>
2021-02-16 18:03:51 +0800

    用例问题

--- src/test/java/com/example/demo/HelloWorldApplicationTests.java
+++ src/test/java/com/example/demo/HelloWorldApplicationTests.java
@@ -1,13 +1,6 @@
 package com.example.demo;
 
 import static org.junit.Assert.*;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.ultimania.dao.UserDAO;
-import org.ultimania.dao.UserDAOImpal;
-import org.ultimania.model.User;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
